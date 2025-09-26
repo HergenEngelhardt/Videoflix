ab7117c4ec40119b0605b6a9c4d438643d1fcbb5
YourName <engelhardt.hergen@gmail.com>
2025-09-26 14:39:02 +0200

    docstring apps

--- auth_app/apps.py
+++ auth_app/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class AuthAppConfig(AppConfig):
+    """Django app configuration for the auth_app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth_app'
