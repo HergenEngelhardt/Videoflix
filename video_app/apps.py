167dd8adfff95418ccf9787f5a912c88dbb887ed
YourName <engelhardt.hergen@gmail.com>
2025-09-26 14:32:37 +0200

    docstring apps

--- video_app/apps.py
+++ video_app/apps.py
@@ -2,8 +2,10 @@ from django.apps import AppConfig
 
 
 class VideoAppConfig(AppConfig):
+    """Django app configuration for the video_app."""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'video_app'
     
     def ready(self):
+        """Import signals module to register signal handlers."""
         import video_app.signals
