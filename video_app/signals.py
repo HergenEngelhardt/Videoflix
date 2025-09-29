28729590b4b4c7e218c44aaedb4d5a2094ab1ead
YourName <engelhardt.hergen@gmail.com>
2025-09-29 09:33:08 +0200

    updated docstrings in signals.py

--- video_app/signals.py
+++ video_app/signals.py
@@ -8,13 +8,15 @@ from .utils import queue_video_conversion, cleanup_hls_files
 def process_video(sender, instance, created, **kwargs):
     """
     Signal to automatically process video after upload.
+    Queues HLS conversion in background when new video is created with file.
     """
     if created and instance.video_file:
         queue_video_conversion(instance)
 
 
 def cleanup_video_file(instance):
-    """Clean up video file safely."""
+    """Clean up video file safely.
+    Removes original video file from filesystem with error handling."""
     if instance.video_file:
         try:
             instance.video_file.delete(save=False)
@@ -23,7 +25,8 @@ def cleanup_video_file(instance):
 
 
 def cleanup_thumbnail_file(instance):
-    """Clean up thumbnail file safely."""
+    """Clean up thumbnail file safely.
+    Removes thumbnail image from filesystem with error handling."""
     if instance.thumbnail:
         try:
             instance.thumbnail.delete(save=False)
@@ -33,7 +36,8 @@ def cleanup_thumbnail_file(instance):
 
 @receiver(post_delete, sender=Video)
 def delete_video_files(sender, instance, **kwargs):
-    """Signal to cleanup files when video is deleted."""
+    """Signal to cleanup files when video is deleted.
+    Removes original video, thumbnail, and all HLS files from storage."""
     cleanup_video_file(instance)
     cleanup_thumbnail_file(instance)
     
