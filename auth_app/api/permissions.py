de5779e9abad9bdc6219582730e8e7e754c5be52
YourName <engelhardt.hergen@gmail.com>
2025-09-29 09:45:09 +0200

    added docstrings to auth permissions

--- auth_app/api/permissions.py
+++ auth_app/api/permissions.py
@@ -6,6 +6,7 @@ class IsOwnerOrReadOnly(BasePermission):
     Custom permission to only allow owners of an object to edit it.
     """
     def has_object_permission(self, request, view, obj):
+        """Allow read access to all, write access only to owners."""
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
 
@@ -17,4 +18,5 @@ class IsAuthenticated(BasePermission):
     Allows access only to authenticated users.
     """
     def has_permission(self, request, view):
+        """Check if user is authenticated."""
         return bool(request.user and request.user.is_authenticated)
