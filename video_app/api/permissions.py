8185ecc29cab021a78ed6727fc948b5a1f242361
YourName <engelhardt.hergen@gmail.com>
2025-09-29 09:44:22 +0200

    added docstrings to video permissions

--- video_app/api/permissions.py
+++ video_app/api/permissions.py
@@ -6,6 +6,7 @@ class IsAuthenticatedForVideo(BasePermission):
     Custom permission to check if user is authenticated for video access.
     """
     def has_permission(self, request, view):
+        """Check if user is authenticated for video access."""
         return bool(request.user and request.user.is_authenticated)
 
 
@@ -14,6 +15,7 @@ class IsAdminOrReadOnly(BasePermission):
     Custom permission to only allow admins to edit videos.
     """
     def has_permission(self, request, view):
+        """Allow authenticated users read access, admins write access."""
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return bool(request.user and request.user.is_authenticated)
         return bool(request.user and request.user.is_staff)
